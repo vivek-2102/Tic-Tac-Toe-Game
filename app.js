cd87779037f097ca40ee434ae0e5b874ff6252e4
vivek-2102 <vivek21iya@gmail.com>
2024-01-20 01:15:52 +0530

    made some changes in app.js

--- app.js
+++ app.js
@@ -2,98 +2,58 @@ let music = new Audio("music.mp3");
 let audioTurn = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
 
-let reset=document.querySelector('#reset');
-reset.addEventListener("click",()=>{
-  location.reload();
-})
-
-let arr = new Array(3);
-for (let i = 0; i < arr.length; i++) {
-  arr[i] = [];
+// music.play();
+const checkWin = ()=>{
+  let boxtext = document.getElementsByClassName('btn');
+  let wins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+  ]
+  wins.forEach(e =>{
+      if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+         let h2=document.querySelector('h2');
+         h2.innerText=`Player ${boxtext[e[0]].innerText} won`;
+         gameover.play();
+         document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+         
+      }
+  })
+}
+ 
+let turn = "X";
+function changeTurn() {
+  return turn === "X" ? "O" : "X";
 }
 let btns = document.querySelectorAll(".btn");
+ 
 for (btn of btns) {
   btn.addEventListener("click", btnpress);
+  
 }
-let count = 0;
-let h2 = document.querySelector("h2");
 
 function btnpress() {
-  audioTurn.play();
   let btn = this;
-  count++;
-  btn.classList.add("addColor");
-  let str = btn.innerText;
-  let x = Number(str[0]);
-  let y = Number(str[1]);
-  if (str == "X" || str == "O") {
-    alert("Cannot play that move");
-    count--;
+  audioTurn.play();
+  if (btn.innerHTML === "") {
+    btn.innerText = turn;
+    turn = changeTurn();
   }
-  
-
-  else if (count % 2 == 0) {
-    arr[x][y] = "X";
-    btn.innerText = "X";
-    h2.innerText = "Player O Turn";
-  } else {
-    arr[x][y] = "O";
-    btn.innerText = "O";
-    h2.innerText = "Player X Turn";
-  }
-  if (winsTheGame()) {
-    gameover.play();
-    if (count % 2 == 0) h2.innerText = "Game Over Player X Wins";
-    else h2.innerText = "Game Over Player O Wins";
-     
-  }
+  checkWin();
 }
 
-let countx = 0;
-let counto = 0;
-function winsTheGame() {
-  for (let j = 0; j <= 2; j++) {
-    for (let i = 0; i <= 2; i++) {
-      if (arr[j][i] == "X") countx++;
-      else if (arr[j][i] == "O") counto++;
-    }
-    if (countx == 3) {
-      console.log("X wins");
-      return true;
-    } else if (counto == 3) {
-      console.log("O wins");
-      return true;
-    }
-    countx = 0;
-    counto = 0;
+function reset(){
+  for(btn of btns)
+  {
+    btn.innerText="";
   }
-  for (let j = 0; j <= 2; j++) {
-    for (let i = 0; i <= 2; i++) {
-      if (arr[i][j] == "X") countx++;
-      else if (arr[i][j] == "O") counto++;
-    }
-    if (countx == 3) {
-      console.log("X wins");
-      return true;
-    } else if (counto == 3) {
-      console.log("O wins");
-      return true;
-    }
-    countx = 0;
-    counto = 0;
-  }
-
-  if (arr[0][0] == "X" && arr[1][1] == "X" && arr[2][2] == "X") {
-    console.log("X wins");
-    return true;
-  } else if (arr[0][0] == "O" && arr[1][1] == "O" && arr[2][2] == "O") {
-    console.log("O wins");
-    return true;
-  } else if (arr[0][2] == "X" && arr[1][1] == "X" && arr[2][0] == "X") {
-    console.log("X wins");
-    return true;
-  } else if (arr[0][2] == "O" && arr[1][1] == "O" && arr[2][0] == "O") {
-    console.log("O wins");
-    return true;
-  }
+  document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
 }
+
+let res=document.getElementById('reset');
+res.addEventListener("click",reset);
